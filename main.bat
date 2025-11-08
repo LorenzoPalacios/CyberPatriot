b3f0a52783d46bc1466c6e8a9d3e6e4325b63355
Lorenzo Palacios <lp902470@gmail.com>
2025-11-08 15:56:41 -0600

    main.bat: will now prompt and restart for elevation
    Signed-off-by: Lorenzo Palacios <lp902470@gmail.com>

--- main.bat
+++ main.bat
@@ -2,28 +2,31 @@
 setlocal enableextensions
 setlocal enabledelayedexpansion
 
+set self_dir=%~dp0
+set lib_dir=%self_dir:~0,-1%\lib
+
 rem - Dependencies -
-set lib_backup=".\lib\backup.bat"
-set lib_patches=".\lib\patches.bat"
-set lib_user=".\lib\user.bat"
-set lib_util=".\lib\util.bat"
-
-rem - Driver Code -
-
-goto :init
-
-:main (
-  call %lib_backup% backup_auditpol
-  exit /b !ERRORLEVEL!
-)
-
-rem - Setup Routines -
+set lib_backup="%lib_dir%\backup.bat"
+set lib_patches="%lib_dir%\patches.bat"
+set lib_user="%lib_dir%\user.bat"
+set lib_util="%lib_dir%\util.bat"
 
 :init (
   call %lib_util% cmd_extensions_available
   if not !ERRORLEVEL! EQU 0 ( exit /b !ERRORLEVEL! )
   call :check_libraries
-  goto :main
+  rem We don't exit upon an error from `check_libraries` since other parts of
+  rem the script could still be operational.
+  call :check_elevation
+  if not !ERRORLEVEL! EQU 0 (
+    powershell -Command "& { Start-Process \"cmd.exe\" -ArgumentList \"/c %~f0\" -verb runas }"
+    exit /b
+  )
+)
+
+:main (
+  call %lib_backup% reg_save
+  exit /b !ERRORLEVEL!
 )
 
 :check_libraries (
@@ -31,4 +34,10 @@ rem - Setup Routines -
   if not exist %lib_patches%  echo init: Library %lib_patches% not found.
   if not exist %lib_user%     echo init: Library %lib_user% not found.
   if not exist %lib_util%     echo init: Library %lib_util% not found.
+  exit /b 0
+)
+
+:check_elevation (
+  bcdedit > nul
+  exit /b %ERRORLEVEL%
 )
